9a49065b909a5407a2b20137a830a020e7c0edc4
Höhl, Lukas <lukas.hoehl@accso.de>
2022-07-05 13:57:22 +0200

    refactor: remove duplicated code
    Signed-off-by: Höhl, Lukas <lukas.hoehl@accso.de>

--- lua/jaq-nvim.lua
+++ lua/jaq-nvim.lua
@@ -36,6 +36,27 @@ local config = {
   }
 }
 
+local function subsitute(cmd)
+  cmd = cmd:gsub("%%", vim.fn.expand('%'));
+  cmd = cmd:gsub("$fileBase", vim.fn.expand('%:r'));
+  cmd = cmd:gsub("$filePath", vim.fn.expand('%:p'));
+  cmd = cmd:gsub("$file", vim.fn.expand('%'));
+  cmd = cmd:gsub("$dir", vim.fn.expand('%:p:h'));
+  cmd = cmd:gsub("$moduleName",
+    vim.fn.substitute(vim.fn.substitute(vim.fn.fnamemodify(vim.fn.expand("%:r"), ":~:."), "/", ".", "g"), "\\", ".",
+      "g"));
+  cmd = cmd:gsub("$altFile", vim.fn.expand('#'))
+  return cmd
+end
+
+local function execute_if_function(cmd) then
+  if type(cmd) == "function" then
+    return cmd()
+  end
+  return cmd
+end
+
+
 local function floatingWin(cmd)
   M.buf = vim.api.nvim_create_buf(false, true)
   local win_height = math.ceil(vim.api.nvim_get_option("lines") * config.ui.float.height - 4)
@@ -60,71 +81,55 @@ function M.setup(user_options) config = vim.tbl_deep_extend('force', config, use
 
 local function internal()
   local cmd = config.cmds.internal[vim.bo.filetype]
-  if cmd ~= nil then
-    -- possibilty for cmd to be a function which returns the cmd string
-    if type(cmd) == "function" then
-      cmd = cmd()
-    end
-    cmd = cmd:gsub("%%", vim.fn.expand('%'));
-    cmd = cmd:gsub("$fileBase", vim.fn.expand('%:r'));
-    cmd = cmd:gsub("$filePath", vim.fn.expand('%:p'));
-    cmd = cmd:gsub("$file", vim.fn.expand('%'));
-    cmd = cmd:gsub("$dir", vim.fn.expand('%:p:h'));
-    cmd = cmd:gsub("$moduleName",
-      vim.fn.substitute(vim.fn.substitute(vim.fn.fnamemodify(vim.fn.expand("%:r"), ":~:."), "/", ".", "g"), "\\", ".",
-        "g"));
-    cmd = cmd:gsub("$altFile", vim.fn.expand('#'))
-    vim.cmd(cmd)
-  else
+  if not cmd then
     vim.cmd("echohl ErrorMsg | echo 'Error: Invalid command' | echohl None")
+    return
   end
+  -- possibilty for cmd to be a function which returns the cmd string
+  cmd = execute_if_function(cmd)
+  cmd = subsitute(cmd)
+  vim.cmd(cmd)
 end
 
 local function run(type)
   local cmd = config.cmds.external[vim.bo.filetype]
-  if cmd ~= nil then
-    cmd = cmd:gsub("%%", vim.fn.expand('%'));
-    cmd = cmd:gsub("$fileBase", vim.fn.expand('%:r'));
-    cmd = cmd:gsub("$filePath", vim.fn.expand('%:p'));
-    cmd = cmd:gsub("$file", vim.fn.expand('%'));
-    cmd = cmd:gsub("$dir", vim.fn.expand('%:p:h'));
-    cmd = cmd:gsub("$moduleName",
-      vim.fn.substitute(vim.fn.substitute(vim.fn.fnamemodify(vim.fn.expand("%:r"), ":~:."), "/", ".", "g"), "\\", ".",
-        "g"));
-    cmd = cmd:gsub("$altFile", vim.fn.expand('#'))
-    if config.ui.autosave then vim.cmd("write") end
-    if type == "float" then
-      floatingWin(cmd)
-    elseif type == "bang" then
-      vim.cmd("!" .. cmd)
-    elseif type == "quickfix" or type == "qf" then
-      vim.cmd('cex system("' .. cmd .. '") | ' .. config.ui.quickfix.position .. ' copen ' .. config.ui.quickfix.size)
-      if config.ui.wincmd then vim.cmd("wincmd p") end
-    elseif type == "term" or type == "terminal" then
-      vim.cmd(config.ui.terminal.position .. " " .. config.ui.terminal.size .. "new | term " .. cmd)
-      M.buf = vim.api.nvim_get_current_buf()
-      vim.api.nvim_buf_set_keymap(M.buf, 'n', '<ESC>', '<C-\\><C-n>:bdelete!<CR>', { silent = true })
-      vim.api.nvim_buf_set_option(M.buf, 'filetype', 'Jaq')
-      if config.ui.startinsert then vim.cmd("startinsert") end
-      if config.ui.wincmd then vim.cmd("wincmd p") end
-      if not config.ui.terminal.line_no then vim.cmd("setlocal nonumber") vim.cmd("setlocal norelativenumber") end
-    elseif type == "toggleterm" then
-      vim.cmd(string.format('TermExec cmd="%s" size=%d direction="%s" go_back=%d', cmd, config.ui.toggleterm.size,
-        config.ui.toggleterm.position, config.ui.wincmd and 1 or 0))
-      if config.ui.startinsert then vim.cmd("startinsert") end
-    elseif type == "fterm" then
-      require("FTerm"):new({
-        cmd = cmd,
-        blend = config.ui.float.blend,
-        auto_close = false,
-        dimensions = { height = config.ui.float.height, width = config.ui.float.width, x = config.ui.float.x,
-          y = config.ui.float.y },
-        border = config.ui.float.border,
-        hl = config.ui.float.float_hl
-      }):open()
-    end
-  else
+  if not cmd then
     vim.cmd("echohl ErrorMsg | echo 'Error: Invalid command' | echohl None")
+    return
+  end
+
+  cmd = execute_if_function(cmd)
+  cmd = subsitute(cmd)
+  if config.ui.autosave then vim.cmd("write") end
+  if type == "float" then
+    floatingWin(cmd)
+  elseif type == "bang" then
+    vim.cmd("!" .. cmd)
+  elseif type == "quickfix" or type == "qf" then
+    vim.cmd('cex system("' .. cmd .. '") | ' .. config.ui.quickfix.position .. ' copen ' .. config.ui.quickfix.size)
+    if config.ui.wincmd then vim.cmd("wincmd p") end
+  elseif type == "term" or type == "terminal" then
+    vim.cmd(config.ui.terminal.position .. " " .. config.ui.terminal.size .. "new | term " .. cmd)
+    M.buf = vim.api.nvim_get_current_buf()
+    vim.api.nvim_buf_set_keymap(M.buf, 'n', '<ESC>', '<C-\\><C-n>:bdelete!<CR>', { silent = true })
+    vim.api.nvim_buf_set_option(M.buf, 'filetype', 'Jaq')
+    if config.ui.startinsert then vim.cmd("startinsert") end
+    if config.ui.wincmd then vim.cmd("wincmd p") end
+    if not config.ui.terminal.line_no then vim.cmd("setlocal nonumber") vim.cmd("setlocal norelativenumber") end
+  elseif type == "toggleterm" then
+    vim.cmd(string.format('TermExec cmd="%s" size=%d direction="%s" go_back=%d', cmd, config.ui.toggleterm.size,
+      config.ui.toggleterm.position, config.ui.wincmd and 1 or 0))
+    if config.ui.startinsert then vim.cmd("startinsert") end
+  elseif type == "fterm" then
+    require("FTerm"):new({
+      cmd = cmd,
+      blend = config.ui.float.blend,
+      auto_close = false,
+      dimensions = { height = config.ui.float.height, width = config.ui.float.width, x = config.ui.float.x,
+        y = config.ui.float.y },
+      border = config.ui.float.border,
+      hl = config.ui.float.float_hl
+    }):open()
   end
 end
 
@@ -137,4 +142,7 @@ function M.Jaq(type)
   end
 end
 
+return M
+return M
+return M
 return M
